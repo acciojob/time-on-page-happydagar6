f2a7e34d3a643ce43070b8ec543ffe561fbf5ba2
Your Name <harshitdagar913@gmail.com>
2026-06-06 15:05:56 +0530

    Add timer functionality to App component to track page duration

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [seconds, setSeconds] = useState(0);
 
-export default App
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div style={{ fontFamily: 'serif', marginTop: '20px', marginLeft: '20px' }}>
+      <p>You've been on this page for {seconds} seconds.</p>
+    </div>
+  );
+};
+
+export default App;
